099249c2dc8379672a096de31d702613dd84385d
ericlatimer <emoney33@gmail.com>
2012-10-31 13:51:39 -0500

    Starting activity from New Deck button click

--- InstaCram/src/com/happykrappy/instacram/MainActivity.java
+++ InstaCram/src/com/happykrappy/instacram/MainActivity.java
@@ -2,9 +2,14 @@ package com.happykrappy.instacram;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -20,8 +25,41 @@ public class MainActivity extends Activity {
         Button newDeckButton = (Button) findViewById(R.id.new_deck);
         newDeckButton.setOnClickListener(new View.OnClickListener() {
         	public void onClick(View v) {
-        		Toast.makeText(MainActivity.this, "New Deck Button Clicked", Toast.LENGTH_SHORT).show();
+        		//Toast.makeText(MainActivity.this, "New Deck Button Clicked", Toast.LENGTH_SHORT).show();
+        		AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+
+        		alert.setTitle("New Deck");
+        		alert.setMessage("Input Deck Name");
+
+        		// Set an EditText view to get user input 
+        		final EditText input = new EditText(MainActivity.this);
+        		alert.setView(input);
+
+        		alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+        		public void onClick(DialogInterface dialog, int whichButton) {
+        		  Editable value = input.getText();
+        		  Deck newDeck = new Deck(value.toString());
+        		  
+        		  //add newDeck to our databases of decks
+        		  
+        		  //redirect user to deck page with First Card Button
+        		  //ViewDeckActivity
+        		  Intent i = new Intent(MainActivity.this, ViewDeckActivity.class);
+        		  i.putExtra("DeckName", value.toString());
+        		  startActivity(i); 
+        		  }
+        		});
+
+        		alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        		  public void onClick(DialogInterface dialog, int whichButton) {
+        		    // Canceled.
+        		  }
+        		});
+
+        		alert.show();
         	}
+        	
+        	
 		});
         Button editDeckButton = (Button) findViewById(R.id.edit_deck);
         editDeckButton.setOnClickListener(new View.OnClickListener() {
