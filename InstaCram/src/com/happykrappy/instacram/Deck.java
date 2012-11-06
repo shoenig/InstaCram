7bbe4144208a1b74f44375a61e6adfaffd54e52d
ericlatimer <emoney33@gmail.com>
2012-11-05 23:17:24 -0600

    Added deck and viewDeckActivity

---
+++ InstaCram/src/com/happykrappy/instacram/Deck.java
@@ -0,0 +1,17 @@
+package com.happykrappy.instacram;
+
+public class Deck {
+	private String name;
+	
+	Deck(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
