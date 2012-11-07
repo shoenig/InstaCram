8542cfcd5009c952369503c96892453436748485
Seth Hoenig <seth.a.hoenig@gmail.com>
2012-11-07 08:06:58 -0600

    things

---
+++ InstaCram/src/com/happykrappy/instacram/DeckManipulator.java
@@ -0,0 +1,11 @@
+package com.happykrappy.instacram;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DeckManipulator extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceBundle) {
+		
+	}
+}
--- InstaCram/src/com/happykrappy/instacram/MainActivity.java
+++ InstaCram/src/com/happykrappy/instacram/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button newDeckButton = (Button) findViewById(R.id.new_deck);
+        newDeckButton = (Button) findViewById(R.id.new_deck);
         newDeckButton.setOnClickListener(new View.OnClickListener() {
         	public void onClick(View v) {
         		//Toast.makeText(MainActivity.this, "New Deck Button Clicked", Toast.LENGTH_SHORT).show();
@@ -61,13 +61,13 @@ public class MainActivity extends Activity {
         	
         	
 		});
-        Button editDeckButton = (Button) findViewById(R.id.edit_deck);
+        editDeckButton = (Button) findViewById(R.id.edit_deck);
         editDeckButton.setOnClickListener(new View.OnClickListener() {
         	public void onClick(View v) {
         		Toast.makeText(MainActivity.this, "Edit Deck Button Clicked", Toast.LENGTH_SHORT).show();
         	}
 		});
-        Button viewDeckButton = (Button) findViewById(R.id.view_deck);
+        viewDeckButton = (Button) findViewById(R.id.view_deck);
         viewDeckButton.setOnClickListener(new View.OnClickListener() {
         	public void onClick(View v) {
         		Toast.makeText(MainActivity.this, "View Deck Button Clicked", Toast.LENGTH_SHORT).show();
