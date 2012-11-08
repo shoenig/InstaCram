228b8c75eb48b03908f22d792db125ac15fadbd5
Seth Hoenig <seth.a.hoenig@gmail.com>
2012-11-07 22:40:09 -0600

    takes photos

--- InstaCram/src/com/happykrappy/instacram/EditDeckActivity.java
+++ InstaCram/src/com/happykrappy/instacram/EditDeckActivity.java
@@ -1,39 +1,136 @@
 package com.happykrappy.instacram;
 
+import java.util.List;
+
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.util.Log;
 
 public class EditDeckActivity extends Activity {
 	
-	TextView name_tv;
+	private static final String TAG = "EditDeckActivity";
+	
+	private TextView mNameTextVew;
+	private Bitmap mFrontThumbnail;
+	private Bitmap mBackThumbnail;
+	private ImageButton mFrontThumbnailImageButton;
+	private ImageButton mBackThumbnailImageButton;
+	private Button mSaveCardButton;
+	private Button mFinishedButton;
+	private static final int RETURN_FROM_FRONT_PHOTO = 11;
+	private static final int RETURN_FROM_BACK_PHOTO = 22;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_deck);
         
-        name_tv = (TextView) findViewById(R.id.deck_name);
-        int DeckId;
+        mNameTextVew = (TextView) findViewById(R.id.deck_name);
+        mFrontThumbnailImageButton = (ImageButton) findViewById(R.id.thumbnail_front);
+        mBackThumbnailImageButton = (ImageButton) findViewById(R.id.thumbnail_back);
+        mSaveCardButton = (Button) findViewById(R.id.save_card);
+        mFinishedButton = (Button) findViewById(R.id.finished);
+        
+        int deckId;
         if (savedInstanceState == null) {
             if(getIntent().getExtras() == null) {
-            	DeckId= 0;
+            	deckId= 0;
             } else {
-            	DeckId = Integer.parseInt(getIntent().getExtras().getString("DeckId"));
+            	deckId = Integer.parseInt(getIntent().getExtras().getString("DeckId"));
             }
         } else {
-        	DeckId= Integer.parseInt((String) savedInstanceState.getSerializable("DeckId"));
+        	deckId= Integer.parseInt((String) savedInstanceState.getSerializable("DeckId"));
         }
         
-        Log.d(MainActivity.TAG, "viewDeck-DeckId: " + DeckId);
-        name_tv.setText("" + DeckId);
+        Log.d(MainActivity.TAG, "viewDeck-DeckId: " + deckId);
+        mNameTextVew.setText("" + deckId); // the key, how to get the name?
+        
+        mFrontThumbnailImageButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Log.i(TAG, "Front Thumbnail Button Clicked, gonna take a picture");
+				dispatchTakePictureIntent(RETURN_FROM_FRONT_PHOTO);
+				Log.i(TAG, "returned from taking photo for front");
+			}
+        });
+        
+        mBackThumbnailImageButton.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		Log.i(TAG, "Back Thumbnail Button Clicked, gonna take a picture");
+        		dispatchTakePictureIntent(RETURN_FROM_BACK_PHOTO);
+        		Log.i(TAG, "returned from taking a photo for back");
+        	}
+        });
+        
+        mSaveCardButton.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		Log.i(TAG, "Save Card Button Clicked, gonna store it and move to next card");
+        	}
+        });
+        
+        mFinishedButton.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		Log.i(TAG, "Finished Button Clicked, gonna stop editing and go to homescreen");
+        	}
+        });
+        
+    }
+    
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    	if(requestCode == RETURN_FROM_FRONT_PHOTO) {
+    		Log.i(TAG, "onActivityResult(), RETURN_FROM_FRONT_PHOTO");
+    		if(resultCode == RESULT_OK) {
+    			// todo: use big image, not thumbnail
+    			setFrontPhoto(intent);
+    		}
+    	} else if(requestCode == RETURN_FROM_BACK_PHOTO) {
+    		Log.i(TAG, "onActivityResult(), RETURN_FROM_BACK_PHOTO");
+    		if(resultCode == RESULT_OK) {
+    			// todo: use big image, not thumbnail
+    			setBackPhoto(intent);
+    		}
+    	}
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_view_deck, menu);
         return true;
+    }
+    
+    private void dispatchTakePictureIntent(int actionCode) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, actionCode);
+    }
+    
+    public static boolean isIntentAvailable(Context context, String action) {
+        final PackageManager packageManager = context.getPackageManager();
+        final Intent intent = new Intent(action);
+        List<ResolveInfo> list =
+                packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+    
+    private void setFrontPhoto(Intent i) {
+    	Bundle extras = i.getExtras();
+    	mFrontThumbnail = (Bitmap) extras.get("data");
+    	mFrontThumbnailImageButton.setImageBitmap(mFrontThumbnail);
+    }    
+    private void setBackPhoto(Intent i) {
+    	Bundle extras = i.getExtras();
+    	mBackThumbnail = (Bitmap) extras.get("data");
+    	mBackThumbnailImageButton.setImageBitmap(mBackThumbnail);
     }
 }
