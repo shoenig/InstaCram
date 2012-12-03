c0cf1d6765e065db258e4af64ba3cf96f8998c05
Seth Hoenig <seth.a.hoenig@gmail.com>
2012-12-02 23:48:04 -0600

    images are B&W

--- InstaCram/src/com/happykrappy/instacram/ImgEdit.java
+++ InstaCram/src/com/happykrappy/instacram/ImgEdit.java
@@ -2,38 +2,50 @@ package com.happykrappy.instacram;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 
 public class ImgEdit {
 
+	
+	/* Takes bmp, a colorful bitmap, and returns a new bitmap
+	 * that is a "black and white" version.
+	 * Not copied off the Internet or anything, 100% Seth!
+	 */
 	public static Bitmap convertToBW(Bitmap bmp) {
-
-		//Bitmap bw = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
-		Bitmap gs = toGrayscale(bmp);
-		
-		int width = gs.getWidth();
-		int height = gs.getHeight();
-		
-		
-		return gs;
-	}
-
-	private static Bitmap toGrayscale(Bitmap bmp) {
-		int height = bmp.getHeight();
 		int width = bmp.getWidth();
-
-		Bitmap gs = Bitmap.createBitmap(width,
-										height,
-										Bitmap.Config.RGB_565);
-		Canvas c = new Canvas(gs);
-		Paint paint = new Paint();
-		ColorMatrix cm = new ColorMatrix();
-		cm.setSaturation(0);
-		ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
-		paint.setColorFilter(f);
-		c.drawBitmap(bmp, 0, 0, paint);
-		return gs;
+		int height = bmp.getHeight();
+		
+		// first calculate average pixel value
+		
+		int[][][] rgb = new int[width][height][4]; // r,g,b,(r+g+b)/3
+		int sum = 0;
+		for(int y=0; y<height; y++) {
+			for(int x=0; x<width; x++) {
+				int color = bmp.getPixel(x, y);
+				rgb[x][y][0] = (color & 0x00FF0000) >> 16; // red
+				rgb[x][y][1] = (color & 0x0000FF00) >> 8;  // green
+				rgb[x][y][2] = (color & 0x000000FF);       // blue
+				rgb[x][y][3] = (rgb[x][y][0] + rgb[x][y][1] + rgb[x][y][2])/3;
+				sum += rgb[x][y][3];
+			}
+		}
+		int total_avg = sum / (width*height);
+		
+		// now set all pixels either black or white
+		// probably need to introduce some sort of threshold modifier thing
+		
+		Bitmap bw = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);		
+		
+		for(int y=0; y<height; y++)
+			for(int x=0; x<width; x++)
+				if(rgb[x][y][3] > total_avg)// set to white
+					bw.setPixel(x, y, Color.WHITE);
+				else
+					bw.setPixel(x, y, Color.BLACK);
+		
+		return bw;
 	}
 }
